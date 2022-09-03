2ec60b4fd04092fcbf0f04052ba02d8bf48f71f3
davimargelo <davimargelo@hotmail.com>
2022-09-02 21:08:20 -0300

    batata melhorada com aleatoriedade no 'queimou' e vencedor

--- src/main/java/queue/labs/QueueEx4.java
+++ src/main/java/queue/labs/QueueEx4.java
@@ -1,40 +1,42 @@
 package queue.labs;
 
-import com.github.javafaker.Faker;
 import lombok.extern.slf4j.Slf4j;
 import queue.data.Person;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Random;
+import java.util.*;
 
 @Slf4j
 public class QueueEx4 {
     public static void main(String[] args) throws InterruptedException {
         Queue<Person> people = new LinkedList<>();
-        Faker faker = new Faker();
 
         people.addAll(List.of(
-                new Person(faker.funnyName().name(), 0),
-                new Person(faker.funnyName().name(), 0),
-                new Person(faker.funnyName().name(), 0),
-                new Person(faker.funnyName().name(), 0)
+                new Person("Cleyton", 0),
+                new Person("Papaleguas", 0),
+                new Person("Jasmine", 0),
+                new Person("Jorge", 0)
         ));
 
         log.info("Pessoas que irão participar: ");
         for (Person person : people) {
-            log.info(person.toString());
+            log.info(person.getName());
         }
 
-        for (int i = 0; i < 10; i++) {
-            Random random = new Random();
-            Integer milliseconds = random.nextInt(2000);
-            Thread.sleep(milliseconds);
+        while (people.size() > 1)
+            rodarABatata(people);
+
+        log.info("O vencedor é " + Objects.requireNonNull(people.peek()).getName());
+    }
+
+    static void rodarABatata(Queue<Person> people) throws InterruptedException {
+        Random random = new Random();
+        Integer potatoTimes = random.nextInt(10);
+        for (int i = 0; i < potatoTimes; i++) {
+            Thread.sleep(1000);
             people.add(people.poll());
-            log.warn("A batata está com " + people.peek());
+            log.warn("A batata está com " + Objects.requireNonNull(people.peek()).getName());
         }
 
-        log.warn("Queimou em " + people.peek());
+        log.warn("Queimou em " + Objects.requireNonNull(people.poll()).getName() + "\n");
     }
 }
